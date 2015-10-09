00798ca3a5154205544eff28750f6059fb63e521
nowakk <saviola19941@wp.pl>
2015-10-09 21:28:38 +0200

    pierwszy commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+ $domains = array('Google.com', 'poc_zta.in.de', 'p.h.0.pl', '1--3.iNfo',
+		'9.xxx', 'face_-_Book.com', 'x.a-b.ww', 'onet..pl', 
+		'canon.photo', '-.net', 'wppl', '!x.pl', 'bmw.', 'onet.12', 
+		'g._.pl', '.wiki.com', 'micro$oft.com', '_.lt', 
+		'wykop.pl.', '.tw', 'p..h.au', 'x.-.uk', 'a.p');
+
+    foreach($domains as $domain) {
+        $ret = preg_match('/^([a-z0-9][a-z0-9_\-]*\.)+[a-z]{2,4}$/i', $domain);
+        echo $domain, ' = ', $ret, '<br>';
+    }
